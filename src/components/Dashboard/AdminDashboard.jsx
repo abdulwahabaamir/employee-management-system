4261ebe2fbed2f3dbce04fc39f006dba90aaa812
Abdul Wahab <abdulwahabbuic1@gmail.com>
2025-08-16 19:48:11 +0500

    display All Task below create new task

--- src/components/Dashboard/AdminDashboard.jsx
+++ src/components/Dashboard/AdminDashboard.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../others/Header";
 import CreateTask from "../others/CreateTask";
+import AllTask from "../others/AllTask";
 
 const AdminDashboard = () => {
   return (
     <div className="bg-gray-100 min-h-screen w-full p-6">
       <Header />
       <CreateTask />
+      <AllTask />
     </div>
   );
 };
---
+++ src/components/others/AllTask.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const AllTask = () => {
+  return (
+    <div className='bg-[#1c1c1c] p-4 rounded overflow-auto h-45 text-white flex flex-col gap-2'>
+        <div className='bg-red-400 rounded flex justify-between py-2 px-4'>
+            <h2>Wahabi</h2>
+            <h3>Make UI of LMS</h3>
+            <h5>Status</h5>
+        </div>
+        <div className='bg-blue-400 rounded flex justify-between py-2 px-4'>
+            <h2>Wahabi</h2>
+            <h3>Make UI of LMS</h3>
+            <h5>Status</h5>
+        </div>
+        <div className='bg-purple-400 rounded flex justify-between py-2 px-4'>
+            <h2>Wahabi</h2>
+            <h3>Make UI of LMS</h3>
+            <h5>Status</h5>
+        </div>
+        <div className='bg-yellow-400 rounded flex justify-between py-2 px-4'>
+            <h2>Wahabi</h2>
+            <h3>Make UI of LMS</h3>
+            <h5>Status</h5>
+        </div>
+        <div className='bg-gray-400 rounded flex justify-between py-2 px-4'>
+            <h2>Wahabi</h2>
+            <h3>Make UI of LMS</h3>
+            <h5>Status</h5>
+        </div>
+        <div className='bg-red-400 rounded flex justify-between py-2 px-4'>
+            <h2>Wahabi</h2>
+            <h3>Make UI of LMS</h3>
+            <h5>Status</h5>
+        </div>
+    </div>
+  )
+}
+
+export default AllTask
